ac36ad3049aae991737408e413178f5bc9d4c8eb
Collin <shuieryin@gmail.com>
2018-03-05 19:55:11 +0800

    add is module

--- lib/util.lua
+++ lib/util.lua
@@ -567,3 +567,18 @@ function scandir(directory, func)
     pfile:close()
     return t
 end
+
+function isModuleAvailable(name)
+    if package.loaded[name] then
+        return true
+    else
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
+    end
+end
